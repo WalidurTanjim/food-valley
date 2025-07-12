3d2e9bbba1a74e82d7e61a0c916094220d0c7dc3
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-12 22:07:28 +0600

    SectionTitle created

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,7 +1,11 @@
+import SectionTitle from "@/components/SectionTitle/SectionTitle";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Food Valley</h1>
+    <div className="home w-full">
+      <div className="container mx-auto w-full px-2 sm:px-6 lg:px-8 py-14">
+        <SectionTitle title={`Our Foods`} />
+      </div>
     </div>
   );
 }
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,9 @@
+const SectionTitle = ({ title }) => {
+    return (
+        <div className="text-center">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-medium text-slate-700">{title}</h1>
+        </div>
+    );
+};
+
+export default SectionTitle;
